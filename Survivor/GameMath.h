8f573ef989a84d6a27eded96803d5ad423ef030a
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2024-01-15 21:55:31 +0900

    [Add] ベクトルクラスを作成

---
+++ Survivor/GameMath.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+/// @brief ベクトル
+struct Vector2
+{
+	float X;
+	float Y;
+
+	Vector2() { X = 0.f; Y = 0.f; }
+	Vector2(float x, float y) { X = x, Y = y; }
+
+	Vector2 operator + (Vector2 vec) {
+		X += vec.X;
+		Y += vec.Y;
+		return *this;
+	}
+
+	Vector2 operator += (Vector2 vec) {
+		X += vec.X;
+		Y += vec.Y;
+		return *this;
+	}
+
+	Vector2 operator * (float f) {
+		X *= f;
+		Y *= f;
+		return *this;
+	}
+
+	void Normalize() {
+		float length = sqrtf(X * X + Y * Y);
+		X = X / length;
+		Y = Y / length;
+	}
+};
